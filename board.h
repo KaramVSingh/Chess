86ba9bfd4cf059752e4c7bb2517d6a213ce14578
Bryan Zeug <bzeug7@gmail.com>
2017-03-09 19:16:09 -0500

    added stuff to chess.c, started defining board.h

--- board.h
+++ board.h
@@ -1,1 +1,10 @@
 //board h file
+
+typedef struct{
+  int player;
+  int difficulty;
+} board_t;
+
+board_t * create_board();
+
+void draw(board_t *board);
---
+++ chess.c
@@ -0,0 +1,16 @@
+#include chess.h //do we need this one?
+#include board.h
+#include player.h
+
+int main(int argc, char** argv){
+
+  if(argc == 0){
+    // full game run
+  }else if(argc == 1){
+    // do something
+  }else{
+    // do something else
+  }
+
+  return 0;
+}
