43b5c94c05fd273119d2c0318086575efec00fa4
Bryan Zeug <bzeug7@gmail.com>
2017-03-09 21:46:12 -0500

    defined preliminary board and player types

--- board.h
+++ board.h
@@ -1,10 +1,13 @@
 //board h file
 
 typedef struct{
-  int player;
-  int difficulty;
+  char board[8][8];
+  //made this a type in case there are other properties we want to add
 } board_t;
 
 board_t * create_board();
 
 void draw(board_t *board);
+
+//might not need this function, but could be handy
+int check(board_t *board);
--- player.h
+++ player.h
@@ -1,1 +1,16 @@
 //player h file
+
+typedef struct player_t {
+  int player_type;
+  //int difficulty; later
+  int piece_type;
+} player_t;
+
+/*
+I think the "move" function should return an int- 1 if everything worked ok,
+0 if there are no valid moves, essentially checkmate. either that or it can
+be void and the check can be delegated to check, but I think this is better
+because it prevents a move being made by the second player if the first can't
+move
+*/
+int move(player_t* player);
