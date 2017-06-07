2039849faab76c07276872e94c4c8a00e2c15fde
Bryan Zeug <bzeug7@gmail.com>
2017-06-06 23:26:00 -0400

    implemented weights array for pieces

--- ai.c
+++ ai.c
@@ -1,7 +1,31 @@
 #include "ai.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+float weights[8][8];
 
 void init_ai(){
+  int i, j;
+  float x, y, val;
+
+  for(i = 0; i < 8; i++){
+    if(i < 4){
+      x = (float) i;
+    }else{
+      x = -1.0*i + 7.0;
+    }
+    for(j = 0; j < 8; j++){
+      if(j < 4){
+        y = (float) j;
+      }else{
+        y = -1.0*j + 7.0;
+      }
+      val = -1.0 + (x + y)/3.0;
+      weights[i][j] = val;
+      //printf("%.2f\t", val);
+    }
+    //printf("\n");
+  }
 
 }
 
