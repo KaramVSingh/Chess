81982d526a6c400136c1994f0d33134c08daec0e
Bryan Zeug <bzeug7@gmail.com>
2017-06-07 00:33:12 -0400

    READ COMMENTS IN make_move, I had a question

--- ai.c
+++ ai.c
@@ -30,11 +30,23 @@ void init_ai(){
 }
 
 int make_move(board_t *board, int color){
-  int i;
+  int i, row, col;
 
   for(i = 0; i < 16; i++){
     if(!board->pieces[color][i].taken){
-
+      row = board->pieces[color][i].row;
+      col = board->pieces[color][i].col;
+      switch(board->pieces[color][i].name){
+        /*
+          might want a comprehensive move function here that can handle taking pieces and updating the
+          board if necessary. Still need to be able to undo it though, maybe store previous moves in a
+          linked list (or maybe just one move rather than all the moves) and have move and undo functions
+          that handle that? Could be a useful thing to have in general for check and for the move function
+          for the player too.
+        */
+        //temp assignment so that the program compiles
+        board->board[row][col] = ' ';
+      }
     }
   }
 
