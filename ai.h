58c014f60d2b1189ff5135124a5682ad60caaf8d
Bryan Zeug <bzeug7@gmail.com>
2017-10-11 10:55:43 -0400

    removed unneeded functions

--- ai.h
+++ ai.h
@@ -6,8 +6,5 @@
 void init_ai();
 int make_move(board_t *board,int difficulty, int color);
 
-void alphabeta(board_t *board, move_t *move, int max, int depth, float alpha, float beta);
-
-float calculate_move(board_t *board, move_t move);
 
 #endif
